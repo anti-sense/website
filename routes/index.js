ce72f7b6d16daca1a39a9ec30eeafac7ca371ac6
Bruno Costa <brunovasquescosta@gmail.com>
2020-12-08 17:55:00 +0000

    Set title and meta for google verification

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var trustifi=require('./../components/emailer/trustifi')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AntiSense - Next-Generation of smelling' });
 });
 
 router.post('/contact/lead', function(req, res, next) {
